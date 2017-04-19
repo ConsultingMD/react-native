2d749836c9aec02418d954be34fa1ef23d8a3e00
Faraz Sherwani <faraz@ipsy.com>
2017-04-19 15:54:40 -0400

    Fix indentation

--- ReactAndroid/src/main/java/com/facebook/react/views/webview/ReactWebViewManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/webview/ReactWebViewManager.java
@@ -213,7 +213,7 @@ public class ReactWebViewManager extends SimpleViewManager<WebView> {
           webView,
           new TopLoadingStartEvent(
               webView.getId(),
-            reactWebView.createWebViewEvent(url)));
+              reactWebView.createWebViewEvent(url)));
     }
 
     protected void emitFinishEvent(WebView webView, String url) {
